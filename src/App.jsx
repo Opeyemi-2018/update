ad115119a45e8d48ab93c5f761ead2a7f82f9547
opeyemi <opeyemiodunayo2018@gmail.com>
2024-08-03 21:57:39 -0700

    fixed classname

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 const App = () => {
   let notes = [
     {
+      id: 1,
       date: "august 26, 2024", photonIQ: "photonIQ", Prerender: "Prerender v1.2.1", whatsnew: "Whats New",
       whatsNewContents: [ "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur." ],
       Improvement: "Improvement",
@@ -11,6 +12,7 @@ const App = () => {
       bugFixContents: [ "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur", "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur."],
     },
     {
+      id: 2,
       date: "august 26, 2024", photonIQ: "photonIQ", Prerender: "Prerender v1.2.1", whatsnew: "Whats New",
       whatsNewContents: [ "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur." ],
       Improvement: "Improvement",
@@ -37,33 +39,33 @@ const App = () => {
         </ul>
       </header>
       <div className="mt-6">
-        <div class="pb-20 md:pb-24 w-full max-w-screen-xl xl:mx-auto ">
-          <div class="relative min-h-[600px] w-full">
-            <div class="absolute bottom-0 left-0 top-2 w-[2px] bg-gray-100 md:left-[25%] after:absolute after:inset-x-0 after:bottom-0 after:h-48 after:bg-gradient-to-b after:from-gray-100 after:to-white"></div>
+        <div className="pb-20 md:pb-24 w-full max-w-screen-xl xl:mx-auto ">
+          <div className="relative min-h-[600px] w-full">
+            <div className="absolute bottom-0 left-0 top-2 w-[2px] bg-gray-100 md:left-[25%] after:absolute after:inset-x-0 after:bottom-0 after:h-48 after:bg-gradient-to-b after:from-gray-100 after:to-white"></div>
             
                 {notes.map((note) => {
-                  let { date, photonIQ, Prerender, whatsnew, whatsNewContents, Improvement, improvementContents, bugFix, bugFixContents } = note;
+                  let { id, date, photonIQ, Prerender, whatsnew, whatsNewContents, Improvement, improvementContents, bugFix, bugFixContents } = note;
                   return (
-                    <div class="ml-6 grid grid-cols-1 pb-20 md:ml-0 md:grid-cols-4 ">
-                      <div class="relative col-span-1 mb-4 flex flex-row  items-center justify-between md:mb-0 md:flex-col md:items-start md:justify-start">
-                        <div class=" text-gray-600 md:mb-1 whitespace-nowrap">{date}</div>
-                        <span class="bg-gray-100 w-fit flex-initial rounded-md px-1.5 py-0.5 text-sm text-gray-600">
+                    <div key={id} className="ml-6 grid grid-cols-1 pb-20 md:ml-0 md:grid-cols-4 ">
+                      <div className="relative col-span-1 mb-4 flex flex-row  items-center justify-between md:mb-0 md:flex-col md:items-start md:justify-start">
+                        <div className=" text-gray-600 md:mb-1 whitespace-nowrap">{date}</div>
+                        <span className="bg-gray-100 w-fit flex-initial rounded-md px-1.5 py-0.5 text-sm text-gray-600">
                           {photonIQ}
                         </span>
-                        <div class="absolute left-[-31px] top-1.5 h-4 w-4 rounded-full border-[2px] border-gray-200 bg-white md:left-auto md:right-[-9px]"></div>
+                        <div className="absolute left-[-31px] top-1.5 h-4 w-4 rounded-full border-[2px] border-gray-200 bg-white md:left-auto md:right-[-9px]"></div>
                       </div>
 
                       
-                      <div class="col-span-3 sm:ml-20 ml-0">
+                      <div className="col-span-3 sm:ml-20 ml-0">
                         <h1 className="text-2xl font-semibold">{Prerender}</h1>
-                        <div class="flex flex-col space-y-2 my-4">
-                          <div class="font-bold">{whatsnew}</div>
-                          <div class="flex flex-col gap-y-3">
-                            <div class="text-base">
-                              {whatsNewContents.map((whatsNewContent) => {
+                        <div className="flex flex-col space-y-2 my-4">
+                          <div className="font-bold">{whatsnew}</div>
+                          <div className="flex flex-col gap-y-3">
+                            <div className="text-base">
+                              {whatsNewContents.map((whatsNewContent, index) => {
                                 return (
-                                  <ul className="ml-9">
-                                    <li class="relative text-[16px]  ml-6 before:absolute before:-left-6 before:top-2 before:h-2 before:w-2 before:rounded-full before:border-2 before:border-gray-300">
+                                  <ul key={index} className="ml-9">
+                                    <li className="relative text-[16px]  ml-6 before:absolute before:-left-6 before:top-2 before:h-2 before:w-2 before:rounded-full before:border-2 before:border-gray-300">
                                       {whatsNewContent}
                                     </li>
                                   </ul>
@@ -72,13 +74,13 @@ const App = () => {
                             </div>
                           </div>
                           
-                          <div class="font-bold">{Improvement}</div>
-                          <div class="flex flex-col gap-y-3">
-                            <div class="text-base">
-                              {improvementContents.map((improvementContent) => {
+                          <div className="font-bold">{Improvement}</div>
+                          <div className="flex flex-col gap-y-3">
+                            <div className="text-base">
+                              {improvementContents.map((improvementContent, index) => {
                                 return (
-                                  <ul className="ml-9">
-                                    <li class="relative text-[16px] ml-6 before:absolute before:-left-6 before:top-2 before:h-2 before:w-2 before:rounded-full before:border-2 before:border-gray-300">
+                                  <ul key={index} className="ml-9">
+                                    <li className="relative text-[16px] ml-6 before:absolute before:-left-6 before:top-2 before:h-2 before:w-2 before:rounded-full before:border-2 before:border-gray-300">
                                       {improvementContent}
                                     </li>
                                   </ul>
@@ -87,13 +89,13 @@ const App = () => {
                             </div>
                           </div>
 
-                          <div class="font-bold">{bugFix}</div>
-                          <div class="flex flex-col gap-y-3">
-                            <div class="text-base">
-                              {bugFixContents.map((bugFixContent) => {
+                          <div className="font-bold">{bugFix}</div>
+                          <div className="flex flex-col gap-y-3">
+                            <div className="text-base">
+                              {bugFixContents.map((bugFixContent, index) => {
                                 return (
-                                  <ul className="ml-9">
-                                    <li class="relative text-[16px] ml-6 before:absolute before:-left-6 before:top-2 before:h-2 before:w-2 before:rounded-full before:border-2 before:border-gray-300">
+                                  <ul key={index} className="ml-9">
+                                    <li className="relative text-[16px] ml-6 before:absolute before:-left-6 before:top-2 before:h-2 before:w-2 before:rounded-full before:border-2 before:border-gray-300">
                                       {bugFixContent}
                                     </li>
                                   </ul>
